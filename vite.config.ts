f414004721cd1639ccfe2b2fbe1ce92c2347b5eb
Nikolay Hoft <51874769+mrHoft@users.noreply.github.com>
2024-05-01 20:55:46 +0300

    feat: ts pathes (#20)

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  build: {
+    assetsDir: './',
+  },
+  resolve: {
+    alias: [
+      {
+        find: '~',
+        replacement: path.resolve(__dirname, './src/'),
+      },
+    ],
+  },
 });
+
