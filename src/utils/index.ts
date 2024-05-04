f6babd20e9ba1aa553dda78125977875ad42723e
mrHoft <ru.hoft@gmail.com>
2024-05-04 17:38:59 +0300

    init: folder structure

---
+++ src/utils/index.ts
@@ -0,0 +1,7 @@
+// Utils
+
+import getSystemTheme from './system';
+
+export { throttle, debounce } from './throttle';
+export { objectEntries, objectKeys, type TEqual } from './types';
+export { getSystemTheme };
