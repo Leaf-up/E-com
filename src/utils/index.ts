0a42c3201ed73dc92dea5f1cbf423add221d111d
mrHoft <ru.hoft@gmail.com>
2024-05-11 05:23:02 +0300

    feat: cookie utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,3 +4,4 @@ export { throttle, debounce } from './throttle';
 export { objectEntries, objectKeys, type TEqual } from './types';
 export { getSystemTheme };
 export { validationRules, checkRules, type TFormValidationErrors } from './validation';
+export { getLifespan, setCookie, getCookie } from './cookie';
