87eef548adfa2d4487cf611812a3005026de4d62
mrHoft <ru.hoft@gmail.com>
2024-05-14 05:27:47 +0300

    fix: lint

--- src/pages/404/404.tsx
+++ src/pages/404/404.tsx
@@ -7,8 +7,8 @@ export default function Page404() {
   return (
     <>
       <h1 className={styles.title}>(404) Page not found!</h1>
-      <img src={srcImg} />
-      <Link className={styles.button__back} to={'/'}>
+      <img src={srcImg} alt="not found" />
+      <Link className={styles.button__back} to="/">
         Home
       </Link>
     </>
--- src/widgets/header/header.tsx
+++ src/widgets/header/header.tsx
@@ -38,7 +38,7 @@ export function Header() {
     <header className={styles.header}>
       <div className={styles.header__content}>
         <div className={styles.header__logo}>
-          <NavLink to={'/'} className={styles.header__logo_img} />
+          <NavLink to="/" className={styles.header__logo_img} />
           <span className={styles.header__logo_title}>E-com</span>
         </div>
         <nav>
