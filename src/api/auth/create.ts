4eaaecc7d3fdf182cd8825ba2e7dca721b5785ff
mrHoft <ru.hoft@gmail.com>
2024-05-17 11:09:28 +0300

    test: api create user

--- src/api/auth/create.ts
+++ src/api/auth/create.ts
@@ -1,47 +1,7 @@
 import type { TRegisterData, TCustomer } from './types';
 import { customerStore } from '~/entities';
 import getToken from './token';
-
-const API_URL = import.meta.env.VITE_CTP_API_URL;
-const PROJECT_KEY = import.meta.env.VITE_CTP_PROJECT_KEY;
-const endpoint = `${API_URL}/${PROJECT_KEY}/customers`;
-
-export function createCustomer(
-  registerData: TRegisterData,
-  token: string,
-): Promise<{ customer: TCustomer | null; error: string | null }> {
-  const info: { status: number; error?: string } = { status: 500 };
-  return fetch(endpoint, {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(registerData),
-  })
-    .then((response) => {
-      info.status = response.status;
-      if (Math.floor(response.status / 100) !== 2) {
-        info.error = response.statusText;
-      }
-      const contentType = response.headers.get('content-type');
-      if (!contentType || !contentType.includes('application/json')) {
-        throw new Error(info.error || 'Havent got a JSON');
-      }
-      return response.json();
-    })
-    .then((data) => {
-      if (Math.floor(info.status / 100) !== 2) {
-        return { customer: null, error: `(${info.status}) ${data.message ?? info.error}` };
-      }
-      const { customer } = data as { customer: TCustomer };
-      return { customer, error: null };
-    })
-    .catch((error) => ({
-      customer: null,
-      error: `(${info.status}) ${error instanceof Error ? error.message : info.error}`,
-    }));
-}
+import createCustomer from './createUser';
 
 export default function performRegister(
   registerData: TRegisterData,
