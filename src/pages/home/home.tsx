1a4ff3490f09f293b01697b36e6af36b65031520
Alisa <Alisa.dragun@mail.ru>
2024-05-20 00:33:07 +0200

    refactor: move navigation to a separate component

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,61 +1,10 @@
-import { NavLink } from 'react-router-dom';
-import { useCustomer } from '~/entities';
+import { Nav } from '~/widgets';
 import styles from './home.module.css';
 
-const menu = [
-  {
-    title: 'Home',
-    route: '/',
-    customer: true,
-  },
-  {
-    title: 'Test',
-    route: '/test',
-    customer: true,
-  },
-  {
-    title: 'Login',
-    route: '/login',
-    customer: false,
-  },
-  {
-    title: 'Register',
-    route: '/register',
-    customer: false,
-  },
-  {
-    title: 'Logout',
-    action: 'logout',
-    customer: true,
-  },
-];
-
 export function Home() {
-  const { user, logout } = useCustomer();
-  const getLinkClass = ({ isActive }: { isActive: boolean }) => (isActive ? styles.nav__link_active : styles.nav__link);
-
   return (
-    <div>
-      <nav className={styles.nav}>
-        <ul className={styles.nav__list}>
-          {menu
-            .filter((el) => (user ? el.customer === Boolean(user) : !el.action))
-            .map((item) => (
-              <li key={item.title}>
-                {item.route && (
-                  <NavLink to={item.route} className={getLinkClass}>
-                    {item.title}
-                  </NavLink>
-                )}
-                {item.action === 'logout' && (
-                  <button type="button" className={styles.nav__link} onClick={logout}>
-                    {item.title}
-                  </button>
-                )}
-              </li>
-            ))}
-        </ul>
-      </nav>
+    <div className={styles.home}>
+      <Nav isColumn />
     </div>
   );
 }
