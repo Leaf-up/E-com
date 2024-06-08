0f2fda794ed1f20e2ca7052e070aee3408689cbc
Alisa Dragun <101509584+1Alisa1@users.noreply.github.com>
2024-06-08 18:50:41 +0200

    feat: Modify quantity of products in cart (#129)
    * feat: modify quantity of products in cart
    
    * fix: replace select with buttons

--- src/ui/card/cartItem/cartItem.tsx
+++ src/ui/card/cartItem/cartItem.tsx
@@ -1,16 +1,71 @@
+import { useState } from 'react';
 import { TLineItem } from '~/api/cart/types';
+import { changeCart } from '~/api';
+import { useCustomer } from '~/entities';
 
 import styles from './.module.css';
 
 export default function CardCart({ item }: { item: TLineItem }) {
+  const { cart } = useCustomer();
+  const [quantity, setQuantity] = useState(item.quantity);
+
+  const price = item.variant.prices[0].discounted
+    ? item.variant.prices[0].discounted.value.centAmount / 10 ** item.variant.prices[0].discounted.value.fractionDigits
+    : item.variant.prices[0].value.centAmount / 100;
+
+  const setQuantityHandler = (n: number) => {
+    setQuantity(n);
+
+    if (cart) {
+      changeCart(cart.id, cart.version, [
+        {
+          action: 'changeLineItemQuantity',
+          productId: item.id,
+          lineItemId: item.id,
+          quantity: n,
+        },
+      ]);
+    }
+  };
+
+  const handleDecreaseButtonClick = () => {
+    if (quantity > 1) {
+      setQuantityHandler(quantity - 1);
+    }
+  };
+
+  const handleIncreaseButtonClick = () => {
+    if (quantity < 30) {
+      setQuantityHandler(quantity + 1);
+    }
+  };
+
   return (
     <div className={styles.cart__item}>
       <img className={styles.cart__item_img} src={item.variant.images[0].url} alt={item.productKey} />
       <div className={styles.cart__item_info}>
-        <span>{item.name['en-US']}</span>
-        <span>{item.variant.prices[0].value.centAmount / 100}$</span>
-        <span>{item.quantity}</span>
-        <span>{item.price.value.centAmount / 100}$</span>
+        <span className={styles.name}>{item.name['en-US']}</span>
+        <span className={styles.price}>{price}$</span>
+        <div className={styles.quantity}>
+          <button
+            type="button"
+            className={styles.quantity__button}
+            onClick={handleDecreaseButtonClick}
+            disabled={quantity === 1 && true}
+          >
+            &#8211;
+          </button>
+          <span>{quantity}</span>
+          <button
+            type="button"
+            className={styles.quantity__button}
+            onClick={handleIncreaseButtonClick}
+            disabled={quantity === 30 && true}
+          >
+            +
+          </button>
+        </div>
+        <span className={styles.price}>{item.totalPrice.centAmount / 100}$</span>
       </div>
     </div>
   );
