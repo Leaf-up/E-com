eb9074ab23e9712161cd0930df4f854505846e63
Alisa <Alisa.dragun@mail.ru>
2024-06-08 22:50:44 +0200

    fix: side menu

--- src/widgets/side-menu/side-menu.tsx
+++ src/widgets/side-menu/side-menu.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import { NavigationMenu } from '~/widgets/navigation/nav';
 import type SideMenuProps from './types';
@@ -5,6 +6,14 @@ import type SideMenuProps from './types';
 import styles from './side-menu.module.css';
 
 export function SideMenu({ isOpen, onClose }: SideMenuProps) {
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+  }, [isOpen]);
+
   return createPortal(
     <div className={`${styles.modal_overlay} ${isOpen && styles.modal_active}`} onClick={onClose} aria-hidden>
       <div
