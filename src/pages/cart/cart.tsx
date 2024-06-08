bb87806951567d088888e0c1e22326d4783515a6
mrHoft <ru.hoft@gmail.com>
2024-06-08 10:54:55 +0300

    feat: cart page

--- src/pages/cart/cart.tsx
+++ src/pages/cart/cart.tsx
@@ -1,3 +1,24 @@
+import { useCustomer } from '~/entities';
+import { CardCart } from '~/ui';
+
 export function Cart() {
-  return <div>Cart page</div>;
+  const { cart } = useCustomer();
+  const cartItems =
+    cart && cart.totalLineItemQuantity ? cart.lineItems.map((item, i) => <CardCart key={i} item={item} />) : [];
+
+  if (cart) {
+    return (
+      <section aria-label="Cart">
+        {cartItems}
+        <p>
+          {'Total: '}
+          <span>{cart.totalLineItemQuantity}</span>
+          {' for '}
+          <span>{cart.totalPrice.centAmount / 100}$</span>
+        </p>
+      </section>
+    );
+  }
+
+  return <p>Cart is empty</p>;
 }
