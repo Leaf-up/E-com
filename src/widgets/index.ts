51f2843a87ca9dfa24c97305d8f0d752acac7795
Nikolay Hoft <51874769+mrHoft@users.noreply.github.com>
2024-06-15 18:54:26 +0300

    feat: Copy component (#139)
    * feat: copy component
    * feat: home page promo code fields

--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -2,8 +2,9 @@ import message from './message/message';
 import Slider from './slider/slider';
 import AddToCart from './cart/add/addToCart';
 import RemoveFromCart from './cart/remove/removeFromCart';
+import { CopyInput } from './copy/copy';
 
-export { Slider, message, AddToCart, RemoveFromCart };
+export { Slider, message, AddToCart, RemoveFromCart, CopyInput };
 export { Header } from './header/header';
 export { NavigationMenu } from './navigation/nav';
 export { LoginForm } from './login-form/login-form';
