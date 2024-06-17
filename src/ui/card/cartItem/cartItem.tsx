eea0cc9fac9fb7932e0d217fe325fc1ac2bec1a8
mrHoft <ru.hoft@gmail.com>
2024-06-17 20:49:52 +0300

    feat: separate loading indicator

--- src/ui/card/cartItem/cartItem.tsx
+++ src/ui/card/cartItem/cartItem.tsx
@@ -8,19 +8,18 @@ import styles from './.module.css';
 
 const trashIcon = '/icons/trash.svg';
 const loadingIcon = '/icons/loading.svg';
+type TLoadingItem = 'minus' | 'plus' | 'del';
 
 export default function CardCart({ item }: { item: TLineItem }) {
   const { cart } = useCustomer();
   const [quantity, setQuantity] = useState(item.quantity);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState<Record<TLoadingItem, boolean>>({ minus: false, plus: false, del: false });
 
   const price = item.variant.prices[0].discounted
     ? item.variant.prices[0].discounted.value.centAmount / 10 ** item.variant.prices[0].discounted.value.fractionDigits
     : item.variant.prices[0].value.centAmount / 100;
 
   const setQuantityHandler = (n: number) => {
-    setLoading(true);
-
     if (cart) {
       changeCart(cart.id, cart.version, [
         {
@@ -32,26 +31,28 @@ export default function CardCart({ item }: { item: TLineItem }) {
       ]).then((response) => {
         if (response.error) message.show(response.error, 'error');
         setQuantity(n);
-        setLoading(false);
+        setLoading((prev) => ({ ...prev, minus: false, plus: false }));
       });
     }
   };
 
   const handleDecreaseButtonClick = () => {
     if (quantity > 1) {
+      setLoading((prev) => ({ ...prev, minus: true }));
       setQuantityHandler(quantity - 1);
     }
   };
 
   const handleIncreaseButtonClick = () => {
     if (quantity < 30) {
+      setLoading((prev) => ({ ...prev, plus: true }));
       setQuantityHandler(quantity + 1);
     }
   };
 
   const removeProductHandler = () => {
     if (cart) {
-      setLoading(true);
+      setLoading((prev) => ({ ...prev, del: true }));
       changeCart(cart.id, cart.version, [
         {
           action: 'removeLineItem',
@@ -60,7 +61,7 @@ export default function CardCart({ item }: { item: TLineItem }) {
       ]).then((response) => {
         if (response.error) message.show(response.error, 'error');
         else message.show('Product was deleted from cart');
-        setLoading(false);
+        setLoading((prev) => ({ ...prev, del: false }));
       });
     }
   };
@@ -80,26 +81,26 @@ export default function CardCart({ item }: { item: TLineItem }) {
             type="button"
             className={styles.quantity__button}
             onClick={handleDecreaseButtonClick}
-            disabled={quantity === 1 || loading}
+            disabled={quantity === 1 || loading.minus || loading.plus}
           >
             <span>&#8211;</span>
-            {loading && <img className={styles.button_loading} src={loadingIcon} alt="" />}
+            {loading.minus && <img className={styles.button_loading} src={loadingIcon} alt="" />}
           </button>
           <span>{quantity}</span>
           <button
             type="button"
             className={styles.quantity__button}
             onClick={handleIncreaseButtonClick}
-            disabled={quantity >= 10 || loading}
+            disabled={quantity >= 10 || loading.minus || loading.plus}
           >
             <span>+</span>
-            {loading && <img className={styles.button_loading} src={loadingIcon} alt="" />}
+            {loading.plus && <img className={styles.button_loading} src={loadingIcon} alt="" />}
           </button>
         </div>
         <span className={styles.price}>{item.totalPrice.centAmount / 100}$</span>
         <div className={styles.trash}>
           <img src={trashIcon} alt="trash" className={styles.trash__icon} onClick={removeProductHandler} aria-hidden />
-          {loading && <img className={styles.button_loading} src={loadingIcon} alt="" />}
+          {loading.del && <img className={styles.button_loading} src={loadingIcon} alt="" />}
         </div>
       </div>
     </div>
